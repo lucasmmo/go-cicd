f4848b7007476fe9b6fae03ada991a2abc3437b0
lucasmmo <lucasmocerinomonteiro@gmail.com>
2021-05-11 19:27:02 -0300

    add: Initial Commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/lucasmmo
+
+go 1.13
+
+require github.com/gofiber/fiber/v2 v2.9.0
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+		c.Send([]byte("hello"))
+		return nil
+	})
+	app.Listen(":3000")
+}
