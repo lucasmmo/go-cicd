dcc3cae524cadc91c35e377535d641d28a5b56db
Lucas Mocerino Monteiro <75847764+lucasmmo@users.noreply.github.com>
2021-05-12 16:35:00 -0300

    update: main.go

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "github.com/gofiber/fiber/v2"
 func main() {
 	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
-		c.Send([]byte("hello world"))
+		c.Send([]byte("Testing webhook to automaticated build"))
 		return nil
 	})
 	app.Listen(":3000")
